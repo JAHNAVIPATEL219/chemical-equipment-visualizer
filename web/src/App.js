0f5eabfdaf148e6c6f36355486e1ee6fb5023047
Jahnavi Chinthanippula <jahnavichinthanippula@gmail.com>
2026-01-30 23:04:49 +0530

    Update React frontend

--- web/src/App.js
+++ web/src/App.js
@@ -35,7 +35,7 @@ function App() {
     setLoading(true);
 
     axios
-      .post("https://chemical-equipment-backend-ikow.onrender.com/api/upload/", formData)
+      .post("http://127.0.0.1:8000/api/upload/", formData)
       .then((res) => {
         setData(res.data);
         setLoading(false);
